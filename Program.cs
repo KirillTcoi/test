6440f2cfae798267a0f3301734ada1f99a12241c
KirillTcoi <tcoidevelopment@gmail.com>
2016-11-15 20:12:14 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int g = 0;
+            int[] Mass = new int[10] { 1, 0, 1, 1, 1, 1, 1, 0, 0, 1 };
+            for (int i = 1; i < 10; i++)
+            {
+                if (Mass[i] == Mass[i-1])
+                {
+                    g++;
+                }
+            }
+            Console.WriteLine(g);
+            Console.ReadKey();
+        }
+    }
+}
